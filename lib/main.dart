570c3456b603574e91ed472139ff79eefe3670a9
vagish1 <35616902+vagish1@users.noreply.github.com>
2022-03-25 08:55:59 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,487 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late final PageController pageController;
+  ScrollController _scrollController = ScrollController();
+  int pageNo = 0;
+
+  Timer? carasouelTmer;
+
+  Timer getTimer() {
+    return Timer.periodic(const Duration(seconds: 3), (timer) {
+      if (pageNo == 4) {
+        pageNo = 0;
+      }
+      pageController.animateToPage(
+        pageNo,
+        duration: const Duration(seconds: 1),
+        curve: Curves.easeInOutCirc,
+      );
+      pageNo++;
+    });
+  }
+
+  @override
+  void initState() {
+    pageController = PageController(initialPage: 0, viewportFraction: 0.85);
+    carasouelTmer = getTimer();
+    _scrollController.addListener(() {
+      if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.reverse) {
+        showBtmAppBr = false;
+        setState(() {});
+      } else {
+        showBtmAppBr = true;
+        setState(() {});
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  bool showBtmAppBr = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          controller: _scrollController,
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 36.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: ListTile(
+                  onTap: () {},
+                  selected: true,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(16.0),
+                    ),
+                  ),
+                  selectedTileColor: Colors.indigoAccent.shade100,
+                  title: Text(
+                    "Welcome Back",
+                    style: Theme.of(context).textTheme.subtitle1!.merge(
+                          const TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 18.0,
+                          ),
+                        ),
+                  ),
+                  subtitle: Text(
+                    "A Greet welcome to you all.",
+                    style: Theme.of(context).textTheme.subtitle2,
+                  ),
+                  trailing: PopUpMen(
+                    menuList: const [
+                      PopupMenuItem(
+                        child: ListTile(
+                          leading: Icon(
+                            CupertinoIcons.person,
+                          ),
+                          title: Text("My Profile"),
+                        ),
+                      ),
+                      PopupMenuItem(
+                        child: ListTile(
+                          leading: Icon(
+                            CupertinoIcons.bag,
+                          ),
+                          title: Text("My Bag"),
+                        ),
+                      ),
+                      PopupMenuDivider(),
+                      PopupMenuItem(
+                        child: Text("Settings"),
+                      ),
+                      PopupMenuItem(
+                        child: Text("About Us"),
+                      ),
+                      PopupMenuDivider(),
+                      PopupMenuItem(
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.logout,
+                          ),
+                          title: Text("Log Out"),
+                        ),
+                      ),
+                    ],
+                    icon: CircleAvatar(
+                      backgroundImage: const NetworkImage(
+                        'https://images.unsplash.com/photo-1644982647869-e1337f992828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
+                      ),
+                      child: Container(),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 200,
+                child: PageView.builder(
+                  controller: pageController,
+                  onPageChanged: (index) {
+                    pageNo = index;
+                    setState(() {});
+                  },
+                  itemBuilder: (_, index) {
+                    return AnimatedBuilder(
+                      animation: pageController,
+                      builder: (ctx, child) {
+                        return child!;
+                      },
+                      child: GestureDetector(
+                        onTap: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content:
+                                  Text("Hello you tapped at ${index + 1} "),
+                            ),
+                          );
+                        },
+                        onPanDown: (d) {
+                          carasouelTmer?.cancel();
+                          carasouelTmer = null;
+                        },
+                        onPanCancel: () {
+                          carasouelTmer = getTimer();
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.only(
+                              right: 8, left: 8, top: 24, bottom: 12),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(24.0),
+                            color: Colors.amberAccent,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: 5,
+                ),
+              ),
+              const SizedBox(
+                height: 12.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: List.generate(
+                  5,
+                  (index) => GestureDetector(
+                    child: Container(
+                      margin: const EdgeInsets.all(2.0),
+                      child: Icon(
+                        Icons.circle,
+                        size: 12.0,
+                        color: pageNo == index
+                            ? Colors.indigoAccent
+                            : Colors.grey.shade300,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(24.0),
+                child: GridB(),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: showBtmAppBr
+          ? FloatingActionButtonLocation.centerDocked
+          : FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+      bottomNavigationBar: AnimatedContainer(
+        child: BottomAppBar(
+          notchMargin: 8.0,
+          shape: const CircularNotchedRectangle(),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.home_outlined,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  CupertinoIcons.heart,
+                ),
+              ),
+              const SizedBox(
+                width: 50,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  CupertinoIcons.cart,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  CupertinoIcons.bell,
+                ),
+              ),
+            ],
+          ),
+        ),
+        duration: const Duration(
+          milliseconds: 800,
+        ),
+        curve: Curves.easeInOutSine,
+        height: showBtmAppBr ? 70 : 0,
+      ),
+    );
+  }
+}
+
+class PopUpMen extends StatelessWidget {
+  final List<PopupMenuEntry> menuList;
+  final Widget? icon;
+  const PopUpMen({Key? key, required this.menuList, this.icon})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      itemBuilder: ((context) => menuList),
+      icon: icon,
+    );
+  }
+}
+
+class FabExt extends StatelessWidget {
+  const FabExt({
+    Key? key,
+    required this.showFabTitle,
+  }) : super(key: key);
+
+  final bool showFabTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.extended(
+      onPressed: () {},
+      label: AnimatedContainer(
+        duration: const Duration(seconds: 2),
+        padding: const EdgeInsets.all(12.0),
+        child: Row(
+          children: [
+            const Icon(CupertinoIcons.cart),
+            SizedBox(width: showFabTitle ? 12.0 : 0),
+            AnimatedContainer(
+              duration: const Duration(seconds: 2),
+              child: showFabTitle ? const Text("Go to cart") : const SizedBox(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GridB extends StatefulWidget {
+  const GridB({Key? key}) : super(key: key);
+
+  @override
+  State<GridB> createState() => _GridBState();
+}
+
+class _GridBState extends State<GridB> {
+  final List<Map<String, dynamic>> gridMap = [
+    {
+      "title": "white sneaker with adidas logo",
+      "price": "\$255",
+      "images":
+          "https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80",
+    },
+    {
+      "title": "Black Jeans with blue stripes",
+      "price": "\$245",
+      "images":
+          "https://images.unsplash.com/photo-1541099649105-f69ad21f3246?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    },
+    {
+      "title": "Red shoes with black stripes",
+      "price": "\$155",
+      "images":
+          "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+      "title": "Gamma shoes with beta brand.",
+      "price": "\$275",
+      "images":
+          "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    },
+    {
+      "title": "Alpha t-shirt for alpha testers.",
+      "price": "\$25",
+      "images":
+          "https://images.unsplash.com/photo-1583743814966-8936f5b7be1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    },
+    {
+      "title": "Beta jeans for beta testers",
+      "price": "\$27",
+      "images":
+          "https://images.unsplash.com/photo-1602293589930-45aad59ba3ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    },
+    {
+      "title": "V&V  model white t shirts.",
+      "price": "\$55",
+      "images":
+          "https://images.unsplash.com/photo-1554568218-0f1715e72254?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    }
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 12.0,
+        mainAxisSpacing: 12.0,
+        mainAxisExtent: 310,
+      ),
+      itemCount: gridMap.length,
+      itemBuilder: (_, index) {
+        return Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(
+              16.0,
+            ),
+            color: Colors.amberAccent.shade100,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(16.0),
+                  topRight: Radius.circular(16.0),
+                ),
+                child: Image.network(
+                  "${gridMap.elementAt(index)['images']}",
+                  height: 170,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "${gridMap.elementAt(index)['title']}",
+                      style: Theme.of(context).textTheme.subtitle1!.merge(
+                            const TextStyle(
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                    ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    Text(
+                      "${gridMap.elementAt(index)['price']}",
+                      style: Theme.of(context).textTheme.subtitle2!.merge(
+                            TextStyle(
+                              fontWeight: FontWeight.w700,
+                              color: Colors.grey.shade500,
+                            ),
+                          ),
+                    ),
+                    const SizedBox(
+                      height: 8.0,
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            CupertinoIcons.heart,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            CupertinoIcons.cart,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
